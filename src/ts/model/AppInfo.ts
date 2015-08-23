3e95d4f5421bb603920377ee5e6f20985ed0f519
solpie <solpie.net@gmail.com>
2015-08-23 08:20:19 +0800

    HScrollBar test

--- src/ts/model/AppInfo.ts
+++ src/ts/model/AppInfo.ts
@@ -16,11 +16,12 @@ class AppInfo extends EventDispatcher {
 
     test() {
         this.newProject();
+        $("#btnTest").on(MouseEvt.CLICK, ()=> {
+            this.projectInfo.curComp.newTrack('D:/projects/animk/test/test60');
+            console.log(this, "test");
+        });
         this.projectInfo.newComp().newTrack('D:/projects/animk/test/test10');
-        //this.projectInfo.newComp().newTrack('D:/projects/animk/test/test30');
-        //this.projectInfo.newComp().newTrack('D:/projects/animk/test/test30');
-        //this.projectInfo.newComp().newTrack('D:/projects/animk/test/test30');
-        //this.projectInfo.newComp().newTrack('D:/projects/animk/test/test30');
+        this.projectInfo.curComp.newTrack('D:/projects/animk/test/test60');
     }
 }
 
