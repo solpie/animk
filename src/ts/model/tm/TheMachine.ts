49aba9169d859aff32100cce8616d583a110ec07
solpie <solpie.net@gmail.com>
2015-09-07 16:34:07 +0800

    clean

--- src/ts/model/tm/TheMachine.ts
+++ src/ts/model/tm/TheMachine.ts
@@ -25,6 +25,7 @@ class TheMachine extends EventDispatcher {
         for (var i = 0; i < this.watchArr.length; i++) {
             var poi:POI = this.watchArr[i];
             poi.psd2png();
+
             //poi.imageInfoArr.map((imageInfo:ImageInfo)=> {
             //    imageInfo.reloadImg();
             //});
@@ -62,7 +63,7 @@ class TheMachine extends EventDispatcher {
                 if (parsingCount < pngDataArr.length)
                     pngDataArr[parsingCount].load(onParsed);
                 else
-                    PsdMaker.convertPNGs2PSD(pngDataArr, appInfo.projectInfo.curComp.width,
+                    this.convertPNGs2PSD(pngDataArr, appInfo.projectInfo.curComp.width,
                         appInfo.projectInfo.curComp.height, "rgba", poi.filename, this.open);
             };
 
@@ -76,6 +77,56 @@ class TheMachine extends EventDispatcher {
             }
             pngDataArr[0].load(onParsed);
         }
+    }
+
+
+    convertPNGs2PSD(pngArr:Array<PngLayerData>, w, h, colorSpace, path:string, pathCallback) {
+        // create psd data
+        var psd = new PsdFile(w, h, colorSpace);
+
+        // append layer
+        var pngLayer:PngLayerData;
+        for (var i = 0; i < pngArr.length; i++) {
+            pngLayer = pngArr[i];
+            var image = new PsdImage(pngLayer.width, pngLayer.height,
+                colorSpace, pngLayer.pixels);
+            var layer = new Layer();
+            layer.drawImage(image);
+            layer.opacity = pngLayer.opacity;
+            psd.appendLayer(layer);
+        }
+
+        // create merged image data
+        var b = new Buffer(4);
+        psd.imageData = new PsdImage(1, 1,
+            colorSpace, b);
+
+        //psd.imageData = new PsdImage(pngLayer.width, pngLayer.height,
+        //    pngLayer.colorSpace, new jDataView(pngLayer.pixels));
+
+        // alpha blend whth white background
+        if (psd.hasAlpha) {
+            //var channels = psd.imageData.channels;
+            //var alphaPixels = channels[channels.length - 1].pixels;
+            //for (var i = 0, l = channels.length - 1; i < l; i++) {
+            //    var pixels = channels[i].pixels;
+            //    for (var index = 0; index < pixels.byteLength; index++) {
+            //        var color = pixels.getUint8(index);
+            //        var alpha = alphaPixels.getUint8(index);
+            //        var blendedColor = this.alphaBlendWithWhite(color, alpha);
+            //        pixels.setUint8(index, blendedColor);
+            //    }
+            //}
+        }
+
+
+        fs.writeFile(path, psd.
+            toBinary(), function (err) {
+            if (err) throw err;
+            pathCallback(path);
+            //console.log(this, "sus", new Date().getTime() - startTime);
+        });
+        //callback(psd.toBinary());
     }
 
     open(path:string) {
